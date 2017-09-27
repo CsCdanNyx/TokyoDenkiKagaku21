9d3155bfdf6014c4c7ec370518f87890dc443fed
chrislu <chrislu30604@gmail.com>
2017-09-27 22:36:18 +0800

    word

---
+++ CsCdanNyx/word.c
@@ -0,0 +1,46 @@
+/*****************K*****************/
+init = (0, 0, 0);
+
+//First sketching
+A = init + (10*sin(pi/6), 10, 10*cos(pi/6));
+B = A + (-185*sin(pi/6), 0, -185*cos(pi/6));
+
+//Second sketching
+C = init + (10*sin(pi/6), 118 + 10, 10*cos(pi/6));
+D = init + (-125*sin(pi/6), -10, -125*cos(pi/6));
+
+//Third sketching
+E = init + (-52*sin(pi/6), 40,-52*cos(pi/6));
+F = init + (-176*sin(pi/6), 130,-176*cos(pi/6));
+
+/*****************N*****************/
+init = (0, 0, 0);
+
+//First sketching
+A = init + (10*sin(pi/6), 10, 10*cos(pi/6));
+B = A + (-185*sin(pi/6), 0, -185*cos(pi/6));
+
+//Second sketching
+C = init + (14*sin(pi/6), 0, 14*cos(pi/6));
+D = init + (-175*sin(pi/6), 126, -175*cos(pi/6));
+
+//Third sketching
+E = init + (10*sin(pi/6), 119, 10*cos(pi/6));
+F = E + (-185*sin(pi/6), 0, -185*cos(pi/6));
+
+/*****************F*****************/
+init = (0, 0, 0);
+
+//First sketching
+A = init + (10*sin(pi/6), 10, 10*cos(pi/6));
+B = A + (-185*sin(pi/6), 0, -185*cos(pi/6));
+
+//Second sketching
+C = init + (-10*sin(pi/6), -10, -10*cos(pi/6));
+D = init + (-10*sin(pi/6), 120, -10*cos(pi/6));
+
+//Third sketching
+E = init + (-80*sin(pi/6), -10, -80*cos(pi/6));
+F = init + (-80*sin(pi/6), 110, -80*cos(pi/6));
+
+/*****************U*****************/
